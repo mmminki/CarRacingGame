0dc973be721c26545a5d8080b9f7480e74a47cce
Minki <cmg1115@naver.com>
2025-02-27 11:57:11 +0900

    feat: 기본 클래스 생성

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Car {
+    private final String name;
+    private int position = 0;   // 이동 거리
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public void move() {
+        Random random = new Random();
+        if(random.nextInt(10) >= 4) {
+            position++;
+            return;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
